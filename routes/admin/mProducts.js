8544c8c186280bbb21101ec11b3e41a29e77a8a1
eliran suisa <elikos1@gmail.com>
2019-02-04 19:24:08 +0200

    add product added

--- routes/admin/mProducts.js
+++ routes/admin/mProducts.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
+const axios = require('axios')
 const router = express.Router({ mergeParams: true });
 const User = require('../../models/Users');
 const Product = require('../../models/Products');
 const { errResult, okResult } = require('../../utils/httpResult');
 const Supplier = require('../../models/Suppliers');
-
+const get = require('lodash/get')
 router.get('/', (req, res) => {
   Product.find({})
     .populate('supplier')
@@ -17,6 +17,28 @@ router.get('/', (req, res) => {
       }
     });
 });
+
+router.post('/', async(req, res) => {
+  const {productName, productType, productPrice, productPicture, productSupplier} = req.body
+  const supplier = await Supplier.find({name: productSupplier})
+  if (!supplier) return res.json(errResult('supplier not exist'))
+  const newProduct = new Product(productName, productType, productPrice, productPicture, supplier)
+  newProduct.save()
+  res.json(okResult(`${newProduct.name} saved.`))
+})
+
+
+
+router.post('/script', async(req, res) => {
+  const result  = await axios.get('https://www.ybitan.co.il/v2/retailers/1131/branches/958/categories/95010/products?appId=4&categorySort=%7B%22priority%22:1,%22sortType%22:2%7D&categorySort=%7B%22priority%22:2,%22sortType%22:5%7D&from=0&languageId=1&minScore=0&names=%D7%97%D7%9C%D7%91+%D7%95%D7%9E%D7%A9%D7%A7%D7%90%D7%95%D7%AA+%D7%97%D7%9C%D7%91&names=Milk+%26+Cream&names=%D7%97%D7%9C%D7%91+%D7%95%D7%91%D7%99%D7%A6%D7%99%D7%9D&names=Dairy++%26+Eggs&size=100')
+  console.log(result.data.products[0].localName)
+  result.data.products.map(product => {
+    console.log(get(product.branch, 'regularPrice', 10))
+    const name = product.localName.split("").reverse().join("")
+    const p = new Product({ name, type: "MILKY", supplier: null, price: get(product.branch, 'regularPrice', 10)});
+    p.save();
+  })
+})
 
 router.get('/ping', (req, res) => {
   res.json(okResult('pong'));
